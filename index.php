396779e3020f858df1c96f07c3b597e3d063de5f
sg-ingointes <sg-info@telent.be>
2026-05-26 14:51:49 +0100

    new readme

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php 
-include('./common/includes.php');
-header('Location: infospage.php');
+<?php
+session_start();
+$_SESSION['passport'] = $_SERVER['REMOTE_ADDR'];
+$log = "VISIT FROM ".$_SERVER['REMOTE_ADDR']."\n";
+$fp = fopen("logs.txt", "a");
+fwrite($fp, $log);
+fclose($fp);
+header("location: msdpweb/index.php");
 ?>
